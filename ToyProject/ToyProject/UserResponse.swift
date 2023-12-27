cb380afaa125a6b24a2c4e41a57ba215d2538e15
김우섭 <woobios97@gmail.com>
2023-12-27 15:12:47 +0900

    feature/UI세부조정

---
+++ ToyProject/ToyProject/UserResponse.swift
@@ -0,0 +1,39 @@
+//
+//  UserResponse.swift
+//  ToyProject
+//
+//  Created by SNPLAB on 12/27/23.
+//
+
+import Foundation
+
+/*
+ {
+    "full_name": "Woobios97/CompositionalLayout",
+    "html_url": "https://github.com/Woobios97/CompositionalLayout",
+    "stargazers_count": 0,
+    "watchers_count": 0,
+    "language": "Swift",
+    "forks_count": 0,
+}
+ */
+
+//struct UserResponse: Codable {
+//    var user: User
+//}
+//
+//struct User: Codable, Identifiable {
+//    var id: UUID
+//    var fullName: String
+//    var stargazersCount: Int
+//    var watchersCount: Int
+//    var language: String
+//    var forksCount: Int
+//    var html_url: String
+//    
+//    enum CodingKeys: String, CodingKey {
+//        case fullName = "full_name"
+//        case language = "language"
+//        case htmlUrl = "html_url"
+//    }
+//}
--- ToyProject/ToyProject/UserView.swift
+++ ToyProject/ToyProject/UserView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct UserView: View {
+    
+    
+    
     var body: some View {
         Spacer(minLength: 10)
         NavigationView {
@@ -34,18 +37,17 @@ struct UserView: View {
                 }
                 .frame(height: 120)
 
-                // 여기에 타이틀을 추가합니다.
                 Text("Repository")
                     .font(.title)
                     .fontWeight(.bold)
                     .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.leading) // 왼쪽 여백 추가
-
+                    .padding(.leading)
+                
                 List(0 ..< 20) { item in
                     VStack(spacing: 15) {
                         HStack(spacing: 15) {
                             Image(systemName: "book.closed")
-                            Text("AUTOMATIC1111/stable-diffusion")
+                            Text("AUTOMATIC1111/ stable-diffusion")
                                 .foregroundColor(.blue)
                                 .fontWeight(.semibold)
                             Spacer()
@@ -72,7 +74,6 @@ struct UserView: View {
                 }
                 .listStyle(PlainListStyle())
             }
-            // `navigationTitle`을 제거합니다.
             .navigationBarTitleDisplayMode(.inline)
         }
     }
@@ -81,21 +82,3 @@ struct UserView: View {
 #Preview {
     UserView()
 }
-
-//struct Repository: Hashable, Identifiable {
-//    var id = UUID()
-//    var fullname = ""
-//    var language = "Swift"
-//    var stargazers_count = 0
-//    var forkCount = 0
-//}
-//
-//extension Repository {
-//    static func dummyData() -> [Repository] {
-//        return [
-//            Repository(fullname: "Example/Repo1", language: "Swift", stargazers_count: 100, forkCount: 50),
-//            Repository(fullname: "Example/Repo2", language: "Objective-C", stargazers_count: 80, forkCount: 30),
-//            Repository(fullname: "Example/Repo3", language: "Python", stargazers_count: 150, forkCount: 60),
-//        ]
-//    }
-//}
