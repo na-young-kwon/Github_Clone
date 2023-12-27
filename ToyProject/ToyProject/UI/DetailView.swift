1d4ed077d212c930d22f4a8593faa127cbc0485f
zoe <74536728+na-young-kwon@users.noreply.github.com>
2023-12-27 15:09:05 +0900

    feat: 웹뷰 UI 구현

---
+++ ToyProject/ToyProject/UI/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  ToyProject
+//
+//  Created by SNPLAB on 12/27/23.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        WebView(url: URL(string: "https://github.com/na-young-kwon")!)
+    }
+}
+
+#Preview {
+    DetailView()
+}
---
+++ ToyProject/ToyProject/UI/WebView.swift
@@ -0,0 +1,24 @@
+//
+//  WebView.swift
+//  ToyProject
+//
+//  Created by SNPLAB on 12/27/23.
+//
+
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    let url: URL
+    
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        let request = URLRequest(url: url)
+        webView.load(request)
+        
+        return webView
+    }
+    
+    func updateUIView(_ webView: WKWebView, context: UIViewRepresentableContext<WebView>) {
+    }
+}
