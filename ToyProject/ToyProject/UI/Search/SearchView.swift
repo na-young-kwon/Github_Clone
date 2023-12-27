f03c2dfa1abfbcdacc4acfe995e7fd3ea81beb47
zoe <74536728+na-young-kwon@users.noreply.github.com>
2023-12-27 14:00:17 +0900

    feat: 최근 검색어 리스트 뷰 구현

--- ToyProject/ToyProject/UI/Search/SearchView.swift
+++ ToyProject/ToyProject/UI/Search/SearchView.swift
@@ -11,11 +11,25 @@ struct SearchView: View {
     @State private var text: String = ""
     
     var body: some View {
-        HStack {
+        VStack(alignment: .leading) {
+            
+            Text("깃헙 ID 검색")
+                .font(.headline)
+                .padding(.bottom, 10)
             ToolBarTextField(text: $text, placeholder: "search..")
                 .frame(height: 40)
+                .padding(.bottom, 20)
+                
+            
+            Text("최근 검색어")
+            List {
+                Text("검색어 1")
+                Text("검색어 2")
+            }
+            .listStyle(.plain)
         }
         .padding(.horizontal)
+        .padding(.top, 20)
     }
 }
 
