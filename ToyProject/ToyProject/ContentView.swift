2e2de275a04d4ab2173250f19b5a774b3e0305c3
김우섭 <woobios97@gmail.com>
2023-12-27 10:04:00 +0900

    First Commit

---
+++ ToyProject/ToyProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToyProject
+//
+//  Created by SNPLAB on 12/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToyProject/ToyProject/ToyProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToyProjectApp.swift
+//  ToyProject
+//
+//  Created by SNPLAB on 12/27/23.
+//
+
+import SwiftUI
+
+@main
+struct ToyProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
