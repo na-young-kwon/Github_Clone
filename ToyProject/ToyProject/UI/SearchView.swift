730b19d55eccc1268207a7fbf82b2b8690cb193e
zoe <74536728+na-young-kwon@users.noreply.github.com>
2023-12-28 11:03:00 +0900

    feat: 서치뷰 화면전환 구현

--- ToyProject/ToyProject/UI/SearchView.swift
+++ ToyProject/ToyProject/UI/SearchView.swift
@@ -9,42 +9,54 @@ import SwiftUI
 
 struct SearchView: View {
     @State private var text: String = ""
+    @State private var isActive = false
     @StateObject var viewModel: SearchViewModel = SearchViewModel()
     
     var body: some View {
-        VStack(alignment: .leading) {
+        NavigationView {
             
-            Text("깃헙 ID 검색")
-                .font(.headline)
-                .padding(.bottom, 10)
-            
-            TextField("search..", text: $text) {
-                viewModel.saveSearch(SearchHistory(text: text))
-            }
-            .frame(height: 40)
-            .padding(.bottom, 20)
-            .textFieldStyle(RoundedBorderTextFieldStyle())
-            .onAppear {
-                UITextField.appearance().clearButtonMode = .whileEditing
-            }
-            
-            Text("최근 검색어")
-            List(viewModel.searchHistory) { data in
-                Text(data.text)
-            }
-            .overlay(
-                Group {
-                    if viewModel.searchHistory.isEmpty {
-                        Text("최근 검색 기록이 없습니다.")
-                    }
+            VStack(alignment: .leading) {
+                
+                NavigationLink(isActive: $isActive) {
+                    UserView()
+                } label: {
+                    EmptyView()
                 }
-            )
-            .listStyle(.plain)
-        }
-        .padding(.horizontal)
-        .padding(.top, 20)
-        .onAppear {
-            viewModel.fetchSearchHistory()
+                
+                Text("깃헙 ID 검색")
+                    .font(.headline)
+                    .padding(.bottom, 10)
+                
+                TextField("search..", text: $text) {
+                    isActive = true
+                    viewModel.saveSearch(SearchHistory(text: text))
+                }
+                .frame(height: 40)
+                .padding(.bottom, 20)
+                .textFieldStyle(.roundedBorder)
+                .onAppear {
+                    UITextField.appearance().clearButtonMode = .whileEditing
+                }
+                
+                Text("최근 검색어")
+                List(viewModel.searchHistory) { data in
+                    Text(data.text)
+                }
+                .overlay(
+                    Group {
+                        if viewModel.searchHistory.isEmpty {
+                            Text("최근 검색 기록이 없습니다.")
+                        }
+                    }
+                )
+                .listStyle(.plain)
+            }
+            .padding(.horizontal)
+            .padding(.top, 20)
+            .onAppear {
+                text = ""
+                viewModel.fetchSearchHistory()
+            }
         }
     }
 }
