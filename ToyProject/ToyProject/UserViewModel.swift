5793d2d2f1012ff1fe4096c8105949de4f6f3b2a
김우섭 <woobios97@gmail.com>
2024-01-03 13:51:17 +0900

    refactor: 주석정리

--- ToyProject/ToyProject/UserViewModel.swift
+++ ToyProject/ToyProject/UserViewModel.swift
@@ -30,38 +30,11 @@ class UserViewModel: ObservableObject {
         isLoading = true
         do {
             user = try await repositoryUseCase.getUser(foruser: username)
-            // 사용자 정보를 가져온 후 아바타 이미지 로드를 시작합니다.
-            if let avatarUrl = user?.avatarUrl {
-//                await loadAvatarImage(forUrl: avatarUrl)
-            }
         } catch {
             print("유저의 종합정보를 받아오는 데 실패했습니다 - \(error)")
         }
         await fetchRepositories(forUser: username) // 여기서도 비동기 호출을 진행합니다.
     }
     
-//    func loadAvatarImage(forUrl avatarUrl: String) async {
-//        if let cachedImage = ImageCache.shared.object(forKey: avatarUrl as NSString) {
-//            self.avatarImage = cachedImage
-//        } else {
-//            do {
-//                if let downloadedImage = try await repositoryUseCase.networkService.downloadImage(from: avatarUrl) {
-//                    ImageCache.shared.setObject(downloadedImage, forKey: avatarUrl as NSString) // 캐시에 이미지 저장
-//                    self.avatarImage = downloadedImage
-//                }
-//            } catch {
-//                print("이미지 다운로드 실패: \(error)")
-//            }
-//        }
-//    }
-    
-    
 }
 
-
-/*
- 해당 페이지에 들어간다면,
- 1. 캐시메모리에서 해당 이미지가 있는 지 체크한다.
- 2. 캐시메모리에서 해당 이미지가 없다면 다운로드한다.
- 3. 다운로드한 이미지를 캐시메모리에 저장한다.
- */
