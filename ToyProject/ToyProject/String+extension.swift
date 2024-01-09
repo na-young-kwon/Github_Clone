8b0f8d77803d9a357507e3db94ae31a1790bf780
zoe <74536728+na-young-kwon@users.noreply.github.com>
2024-01-09 15:11:09 +0900

    chore: getLocalizedString이 배열을 받도록 수정

--- ToyProject/ToyProject/String+extension.swift
+++ ToyProject/ToyProject/String+extension.swift
@@ -11,7 +11,7 @@ extension String {
     func getLocalizedString() -> String {
         return NSLocalizedString(self, comment: "") // comment는 걍 주석임. ""로 비워둬도 되고, key가 어떤 역할인지 적어두면 가독성 향상
     }
-    func getLocalizedString(with argument: CVarArg) -> String {
+    func getLocalizedString(with argument: CVarArg...) -> String {
         return String(format: self.getLocalizedString(), argument)
     }
 }
