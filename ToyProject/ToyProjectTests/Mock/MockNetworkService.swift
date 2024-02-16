72c4e7d2c60ec9746f4934fe6483171a67580d24
권나영 <kny0651@snplab.io>
2024-02-16 17:07:06 +0900

    feat: MockNetworkService 구현

--- ToyProject/ToyProjectTests/Mock/MockNetworkService.swift
+++ ToyProject/ToyProjectTests/Mock/MockNetworkService.swift
@@ -9,10 +9,34 @@ import Foundation
 
 class MockNetworkService: NetworkServiceDelegate {
     func fetchUser(forUser username: String) async throws -> UserDTO {
-        <#code#>
+        let user = UserDTO(id: 383316,
+                           userName: "test",
+                           avatarUrl: "https://avatars.githubusercontent.com/u/383316?v=4",
+                           followers: 48,
+                           following: 0,
+                           bio: "hello world")
+        return user
     }
     
     func fetchRepositories(forUser username: String) async throws -> [RepositoryDTO] {
-        <#code#>
+        let repository = [
+            RepositoryDTO(id: 12196274,
+                          user: RepositoryDTO.User(userID: 383316),
+                          fullName: "test/HelloWorld",
+                          htmlUrl: "https://github.com/test",
+                          starsCount: 6,
+                          watchersCount: 6,
+                          forksCount: 33,
+                          language: "swift"),
+            RepositoryDTO(id: 9872654,
+                          user: RepositoryDTO.User(userID: 383316),
+                          fullName: "test/rokehan",
+                          htmlUrl: "https://github.com/test",
+                          starsCount: 1,
+                          watchersCount: 1,
+                          forksCount: 3,
+                          language: "PHP")
+        ]
+        return repository
     }
 }
---
+++ ToyProject/ToyProjectTests/Mock/MockRepositoryDAO.swift
@@ -0,0 +1,22 @@
+//
+//  MockRepositoryDAO.swift
+//  ToyProjectTests
+//
+//  Created by nayoung kwon on 2/16/24.
+//
+
+import Foundation
+
+class MockRepositoryDAO: RepositoryDAODelegate {
+    func create(_ repositoriesVO: [RepositoryVO]) {
+        <#code#>
+    }
+    
+    func read(_ userID: Int) -> [RepositoryVO] {
+        <#code#>
+    }
+    
+    func delete(_ userID: Int) {
+        <#code#>
+    }
+}
